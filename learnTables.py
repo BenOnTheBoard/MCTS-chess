d637b51faa9be8492c295f597b33ecaaa32c557c
Ben Whitehead <2732382W@student.gla.ac.uk>
2025-03-16 22:26:38 +0000

    Learning piece_tables from stockfish games

--- learnTables.py
+++ learnTables.py
@@ -3,51 +3,63 @@ from copy import deepcopy
 import chess
 import chess.engine
 import numpy as np
+from random import choice
+from tqdm import tqdm
 
 from engine.heuristics.tableBased.pieceTable import PieceTable
 from engine.heuristics.tableBased.tables import TABLES
 
-L_RATE = 200
+L_RATE = 0.001
+ROUNDS = 5000
+RAND_START_DEPTH = 6
+SF_SEARCH_DEPTH = 8
 stockfish = chess.engine.SimpleEngine.popen_uci(
     r"stockfish\stockfish-windows-x86-64-avx2.exe"
 )
-
 tables = deepcopy(TABLES)
 
-for round in range(1000):
-    print(round)
-    board = chess.Board()
 
-    game_history = []
-    while not board.is_game_over():
-        result = stockfish.play(board, chess.engine.Limit(depth=20))
-        board.push(result.move)
-        game_history.append(
-            (
-                board.fen(),
-                PieceTable(tables).evaluate(board),
-                stockfish.analyse(board, chess.engine.Limit(depth=20)),
+def setup_board():
+    start = chess.Board()
+    for _ in range(RAND_START_DEPTH):
+        if not start.is_game_over():
+            start.push(choice(list(start.generate_legal_moves())))
+    return start
+
+
+def sf_self_play(board):
+    game_history = [(chess.Board().fen(), 0, 0)]
+    while not board.is_game_over() and len(board.piece_map()) > 8:
+        eval_dict = stockfish.analyse(board, chess.engine.Limit(depth=SF_SEARCH_DEPTH))
+        sf_eval = eval_dict["score"].white().score()
+        sf_move = eval_dict["pv"][0]
+        board.push(sf_move)
+
+        if sf_eval is not None:
+            game_history.append(
+                (board.fen(), PieceTable(tables).evaluate(board), sf_eval)
             )
-        )
+    return game_history
+
+
+for round in tqdm(range(ROUNDS)):
+    board = setup_board()
+    game_history = sf_self_play(board)
 
     for fen, v, r in game_history:
-        print(f"{fen}\t\t{v}\t{r}")
         l_state = chess.Board(fen=fen)
         for square in chess.SQUARES:
             row, col = divmod(square, 8)
             piece = l_state.piece_at(square)
             if piece is not None:
                 piece_table = tables[piece.piece_type]
-                l_step = L_RATE * (r - v) * v * (1 - v)
-                if l_state.turn == chess.WHITE:
+                l_step = L_RATE * (r - v)
+                if piece.color == chess.WHITE:
                     piece_table[7 - row][col] += l_step
-                elif l_state.turn == chess.BLACK:
+                elif piece.color == chess.BLACK:
                     piece_table[row][col] -= l_step
 
-    if round % 10 == 0:
-        np.set_printoptions(precision=3)
-        for table in tables.values():
-            print(table)
-            print()
-
-    quit()
+np.set_printoptions(precision=0, suppress=True)
+for table in tables.values():
+    print(table)
+    print()
--- versus.py
+++ versus.py
@@ -19,15 +19,20 @@ parameters = {"fen": None, "depth": 12}
 
 board = chess.Board()
 
-mcts = MCTS(board, 30, UCT(0.2), PieceTable())
+mcts = MCTS(board, 20, UCT(0.4), PieceTable())
 while not mcts.position.is_game_over():
     white_choice = mcts.get_move()
     print(mcts.root_node.visits)
-    print([child.move.uci() for child in mcts.root_node.children])
+    print("Move analysis:")
+    for child in mcts.root_node.children:
+        print(
+            f"{child.move.uci()}\t{child.score:.2f}\t{child.visits}\t{(child.score / child.visits):.2f}"
+        )
     mcts.add_move(white_choice)
 
     print()
     print(mcts.position)
+    break
 
     parameters["fen"] = mcts.position.fen()
     response = requests.get(url, params=parameters)
@@ -35,4 +40,5 @@ while not mcts.position.is_game_over():
     mcts.add_move(black_choice)
 
     print()
+    print(black_choice)
     print(mcts.position)
