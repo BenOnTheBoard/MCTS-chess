7aa4e3386822e04de43bf20a46e3864f5c8bdbc9
Ben Whitehead <2732382W@student.gla.ac.uk>
2025-03-19 00:55:03 +0000

    table learning improvements

--- learnTables.py
+++ learnTables.py
@@ -2,21 +2,20 @@ from copy import deepcopy
 
 import chess
 import chess.engine
+from math import log10
 import numpy as np
 from random import choice
 from tqdm import tqdm
 
 from engine.heuristics.tableBased.pieceTable import PieceTable
-from engine.heuristics.tableBased.tables import TABLES
+from engine.heuristics.tableBased.tables import BLANK_TABLES, TABLES
 
-L_RATE = 0.001
-ROUNDS = 5000
 RAND_START_DEPTH = 6
-SF_SEARCH_DEPTH = 8
+SF_SEARCH_DEPTH = 20
 stockfish = chess.engine.SimpleEngine.popen_uci(
     r"stockfish\stockfish-windows-x86-64-avx2.exe"
 )
-tables = deepcopy(TABLES)
+DATA_FILENAME = "training_data.txt"
 
 
 def setup_board():
@@ -28,38 +27,71 @@ def setup_board():
 
 
 def sf_self_play(board):
-    game_history = [(chess.Board().fen(), 0, 0)]
-    while not board.is_game_over() and len(board.piece_map()) > 8:
+    while not board.is_game_over() and board.ply() < 80:
         eval_dict = stockfish.analyse(board, chess.engine.Limit(depth=SF_SEARCH_DEPTH))
         sf_eval = eval_dict["score"].white().score()
         sf_move = eval_dict["pv"][0]
         board.push(sf_move)
 
         if sf_eval is not None:
-            game_history.append(
-                (board.fen(), PieceTable(tables).evaluate(board), sf_eval)
-            )
-    return game_history
+            with open(DATA_FILENAME, "a") as data_file:
+                data_file.write(f"{board.fen()}, {sf_eval}\n")
 
 
-for round in tqdm(range(ROUNDS)):
-    board = setup_board()
-    game_history = sf_self_play(board)
+def generate(games):
+    for _ in tqdm(range(games)):
+        board = setup_board()
+        sf_self_play(board)
 
-    for fen, v, r in game_history:
-        l_state = chess.Board(fen=fen)
-        for square in chess.SQUARES:
-            row, col = divmod(square, 8)
-            piece = l_state.piece_at(square)
-            if piece is not None:
-                piece_table = tables[piece.piece_type]
-                l_step = L_RATE * (r - v)
-                if piece.color == chess.WHITE:
-                    piece_table[7 - row][col] += l_step
-                elif piece.color == chess.BLACK:
-                    piece_table[row][col] -= l_step
 
-np.set_printoptions(precision=0, suppress=True)
-for table in tables.values():
-    print(table)
-    print()
+def train(tables, l_rate):
+    model = PieceTable(tables)
+    changes = deepcopy(BLANK_TABLES)
+    error = 0
+    with open(DATA_FILENAME, "r") as data_file:
+        dataset = data_file.readlines()
+        for line in dataset:
+            fen, r = line.strip().split(",")
+            r = int(r)
+            l_state = chess.Board(fen=fen)
+            v = model.evaluate(l_state)
+            error += (r - v) ** 2
+
+            for square in chess.SQUARES:
+                row, col = divmod(square, 8)
+                piece = l_state.piece_at(square)
+                if piece is not None:
+                    piece_table = changes[piece.piece_type]
+                    l_step = l_rate * (r - v)
+                    if piece.color == chess.WHITE:
+                        piece_table[7 - row][col] += l_step
+                    elif piece.color == chess.BLACK:
+                        piece_table[row][col] -= l_step
+
+    for piece, table in tables.items():
+        changes[piece] /= len(dataset)
+        table += changes[piece]
+
+    error /= len(dataset)
+    return error
+
+
+def pprint_tables(tables):
+    np.set_printoptions(precision=0, suppress=True)
+    for table in tables.values():
+        print(table)
+        print()
+
+
+def main():
+    l_rate = 2
+    rounds = 2_000
+    tables = deepcopy(TABLES)
+    for round in tqdm(range(rounds)):
+        error = train(tables, l_rate)
+        print(f"\nRound: {round}, Error:{error} = 10 ^ {log10(error)}")
+    pprint_tables(tables)
+
+
+if __name__ == "__main__":
+    main()
