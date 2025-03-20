c1dd4c28e1af35c8ffeaa5bca1d57a9d9ed581cb
Ben Whitehead <2732382W@student.gla.ac.uk>
2025-03-20 19:10:17 +0000

    Clean-up and output losses per round of training

--- learnWeights.py
+++ learnWeights.py
@@ -9,30 +9,31 @@ def main():
 
     filename = "training_data.txt"
     loss_fn = torch.nn.MSELoss(reduction="sum")
-    rounds = 2000
-    learning_rate = 1e-6
+    rounds = 100
+    learning_rate = 1e-5
+
+    with open(filename, "r") as data_file:
+        dataset = data_file.readlines()
 
     for round in range(rounds):
         total_loss = 0
-        with open(filename, "r") as data_file:
-            dataset = data_file.readlines()
-            for line in dataset:
-                fen, y = line.strip().split(",")
-                l_state = chess.Board(fen=fen)
+        for line in dataset:
+            fen, y = line.strip().split(",")
+            l_state = chess.Board(fen=fen)
 
-                y = torch.as_tensor(int(y), dtype=torch.float)
-                y_pred = BNet.evaluate(l_state)
+            y = torch.as_tensor(int(y), dtype=torch.float)
+            y_pred = BNet.evaluate(l_state)
 
-                loss = loss_fn(y_pred, y)
-                total_loss += loss
+            loss = loss_fn(y_pred, y)
+            total_loss += loss
 
-                BNet.model.zero_grad()
-                loss.backward()
+            BNet.model.zero_grad()
+            loss.backward()
 
-                with torch.no_grad():
-                    for param in BNet.model.parameters():
-                        param -= learning_rate * param.grad
-        print(f"Round: {round}, Total Loss: {total_loss}")
+            with torch.no_grad():
+                for param in BNet.model.parameters():
+                    param -= learning_rate * param.grad
+        print(f"Round: {round + 1}, Average Loss: {total_loss / len(dataset)}")
 
 
 if __name__ == "__main__":
