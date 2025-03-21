2a11476339d905190d5910d01e92d977bfa0a036
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-03-21 18:10:58 +0000

    Basis for handling testing data

--- learnWeights.py
+++ learnWeights.py
@@ -1,3 +1,5 @@
+from math import log10
+
 import chess
 import torch
 
@@ -8,8 +10,9 @@ def main():
     BNet = BasicNetwork()
 
     filename = "training_data.txt"
+    tests = "testing_data.txt"
     loss_fn = torch.nn.MSELoss(reduction="sum")
-    rounds = 100
+    rounds = 50
     learning_rate = 1e-5
 
     with open(filename, "r") as data_file:
@@ -21,7 +24,10 @@ def main():
             fen, y = line.strip().split(",")
             l_state = chess.Board(fen=fen)
 
-            y = torch.as_tensor(int(y), dtype=torch.float)
+            y = torch.as_tensor(
+                [int(y)],
+                dtype=torch.float,
+            )
             y_pred = BNet.evaluate(l_state)
 
             loss = loss_fn(y_pred, y)
@@ -33,7 +39,25 @@ def main():
             with torch.no_grad():
                 for param in BNet.model.parameters():
                     param -= learning_rate * param.grad
-        print(f"Round: {round + 1}, Average Loss: {total_loss / len(dataset)}")
+        print(f"Round: {round + 1},\tAverage Loss: {total_loss / len(dataset)}")
+
+    with open(tests, "r") as test_data_file:
+        testset = test_data_file.readlines()
+
+    total_loss = 0
+    for line in testset:
+        fen, y = line.strip().split(",")
+        l_state = chess.Board(fen=fen)
+
+        y = torch.as_tensor(int(y), dtype=torch.float)
+        y_pred = BNet.evaluate(l_state)
+
+        loss = loss_fn(y_pred, y)
+        total_loss += loss
+
+    test_result = total_loss / len(testset)
+    print(f"\n\nTest, Average Loss:\t{test_result} = 10 ^ {log10(test_result)}")
+    print(BNet.evaluate(chess.Board()))
 
 
 if __name__ == "__main__":
