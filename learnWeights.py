5fb27ba38cd0dc0cf46f43fe448b20886957a11c
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2025-03-22 05:03:58 +0000

    Testing information at even round of learning

--- learnWeights.py
+++ learnWeights.py
@@ -1,5 +1,3 @@
-from math import log10
-
 import chess
 import torch
 
@@ -13,11 +11,14 @@ def main():
     tests_filename = "testing_data.txt"
     output_filename = "saved_model.pt"
     loss_fn = torch.nn.MSELoss()
-    rounds = 10
+    rounds = 20
     learning_rate = 1e-5
 
     with open(data_filename, "r") as data_file:
         dataset = data_file.readlines()
+
+    with open(tests_filename, "r") as test_data_file:
+        testset = test_data_file.readlines()
 
     for round in range(rounds):
         total_loss = 0
@@ -40,28 +41,24 @@ def main():
             with torch.no_grad():
                 for param in BNet.model.parameters():
                     param -= learning_rate * param.grad
-        print(f"Round: {round + 1},\tAverage Loss: {total_loss / len(dataset)}")
 
-    with open(tests_filename, "r") as test_data_file:
-        testset = test_data_file.readlines()
+        test_loss = 0
+        for line in testset:
+            fen, y = line.strip().split(",")
+            l_state = chess.Board(fen=fen)
 
-    total_loss = 0
-    for line in testset:
-        fen, y = line.strip().split(",")
-        l_state = chess.Board(fen=fen)
+            y = torch.as_tensor(
+                [int(y)],
+                dtype=torch.float,
+            )
+            y_pred = BNet.tensor_eval(l_state)
 
-        y = torch.as_tensor(
-            [int(y)],
-            dtype=torch.float,
+            loss = loss_fn(y_pred, y)
+            test_loss += loss
+
+        print(
+            f"Avg. Training Loss: {total_loss / len(dataset):.2f}\t\tAvg. Test Loss: {test_loss / len(testset):.2f}"
         )
-        y_pred = BNet.tensor_eval(l_state)
-
-        loss = loss_fn(y_pred, y)
-        total_loss += loss
-
-    test_result = total_loss / len(testset)
-    print(f"\n\nTest, Average Loss:\t{test_result} = 10 ^ {log10(test_result)}")
-    print(BNet.evaluate(chess.Board()))
 
     torch.save(BNet.model, output_filename)
 
