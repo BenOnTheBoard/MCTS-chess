1a0c70bef40d076222e50a56c4c9a8d67d8c5346
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2025-03-22 03:07:16 +0000

    Adding saving of model

--- learnWeights.py
+++ learnWeights.py
@@ -9,13 +9,14 @@ from engine.heuristics.basicNetwork import BasicNetwork
 def main():
     BNet = BasicNetwork()
 
-    filename = "training_data.txt"
-    tests = "testing_data.txt"
-    loss_fn = torch.nn.MSELoss(reduction="sum")
-    rounds = 50
+    data_filename = "training_data.txt"
+    tests_filename = "testing_data.txt"
+    output_filename = "saved_model.pt"
+    loss_fn = torch.nn.MSELoss()
+    rounds = 0
     learning_rate = 1e-5
 
-    with open(filename, "r") as data_file:
+    with open(data_filename, "r") as data_file:
         dataset = data_file.readlines()
 
     for round in range(rounds):
@@ -41,7 +42,7 @@ def main():
                     param -= learning_rate * param.grad
         print(f"Round: {round + 1},\tAverage Loss: {total_loss / len(dataset)}")
 
-    with open(tests, "r") as test_data_file:
+    with open(tests_filename, "r") as test_data_file:
         testset = test_data_file.readlines()
 
     total_loss = 0
@@ -49,7 +50,10 @@ def main():
         fen, y = line.strip().split(",")
         l_state = chess.Board(fen=fen)
 
-        y = torch.as_tensor(int(y), dtype=torch.float)
+        y = torch.as_tensor(
+            [int(y)],
+            dtype=torch.float,
+        )
         y_pred = BNet.evaluate(l_state)
 
         loss = loss_fn(y_pred, y)
@@ -58,6 +62,8 @@ def main():
     test_result = total_loss / len(testset)
     print(f"\n\nTest, Average Loss:\t{test_result} = 10 ^ {log10(test_result)}")
     print(BNet.evaluate(chess.Board()))
+
+    torch.save(BNet.model, output_filename)
 
 
 if __name__ == "__main__":
