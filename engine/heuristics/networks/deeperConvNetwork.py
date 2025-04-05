dcbb057354fcc2f1cea0d3d5eb48d25f6732cfdf
BenOnTheBoard <ben.pi.whitehead@gmail.com>
2025-04-05 19:30:29 +0100

    potentially excessive deeper convolutional network

---
+++ engine/heuristics/networks/deeperConvNetwork.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+from engine.heuristics.networks.abstractNetwork import AbstractNetwork
+
+
+class DeeperConvNetwork(AbstractNetwork):
+    def __init__(self):
+        super().__init__()
+
+    def init_model(self):
+        self.model = nn.Sequential(
+            nn.Conv2d(12, 12, (8, 8), padding=(7, 7)),
+            nn.MaxPool2d(2, padding=1),
+            nn.Conv2d(12, 12, (8, 8), padding=(7, 7)),
+            nn.MaxPool2d(2, padding=1),
+            nn.Conv2d(12, 12, (8, 8), padding=(7, 7)),
+            nn.MaxPool2d(2, padding=1),
+            nn.Conv2d(12, 12, (8, 8), padding=(7, 7)),
+            nn.MaxPool2d(2, padding=1),
+            nn.Flatten(),
+            nn.Linear(768, 64),
+            nn.SiLU(),
+            nn.Linear(64, 64),
+            nn.SiLU(),
+            nn.Linear(64, 1),
+            nn.Sigmoid(),
+        )
+
+    @staticmethod
+    def board_to_tensor(state):
+        tensor = super().board_to_tensor(state)
+        return tensor.view(1, 12, 8, 8)
