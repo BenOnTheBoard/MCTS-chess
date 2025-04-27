ce9e581a42345b7003ba927d72dc28014a38f10a
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-04-27 07:18:53 +0100

    simplified CNN

--- engine/heuristics/networks/convNetwork.py
+++ engine/heuristics/networks/convNetwork.py
@@ -6,16 +6,16 @@ from engine.heuristics.networks.abstractNetwork import AbstractNetwork
 class ConvNetwork(AbstractNetwork):
     def init_model(self):
         self.model = nn.Sequential(
-            nn.Conv2d(6, 32, (3, 3), padding=1),
+            nn.Conv2d(6, 16, (3, 3), padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, (3, 3), padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32, 64, (3, 3), padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
+            nn.Dropout(0.5),
             nn.AdaptiveAvgPool2d((2, 2)),
             nn.Flatten(),
-            nn.Linear(256, 256),
+            nn.Linear(128, 256),
             nn.ReLU(),
             nn.Linear(256, 1),
             nn.Sigmoid(),
