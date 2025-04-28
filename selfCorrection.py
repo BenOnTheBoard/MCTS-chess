22f80c035417d417caac94491590fe963e4ec189
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-04-28 20:15:02 +0100

    Self Training Program V1

---
+++ selfCorrection.py
@@ -0,0 +1,87 @@
+import chess
+import chess.engine
+import torch
+
+from engine.heuristics.networks.convNetwork import ConvNetwork
+from engine.mcts import MCTS
+from engine.treeEvaluators.UCT import UCT
+
+
+def process_batch(model, batch, loss_fn):
+    board_tensor, targets = batch
+    board_tensor = board_tensor.to(torch.float32)
+    board_tensor = board_tensor.view(len(board_tensor), 6, 8, 8)
+
+    predictions = model(board_tensor)
+    loss = loss_fn(predictions, targets)
+
+    return loss
+
+
+def train_epoch(model, batch, loss_fn, l_rate):
+    loss = process_batch(model, batch, loss_fn)
+
+    model.zero_grad()
+    loss.backward()
+
+    with torch.no_grad():
+        for param in model.parameters():
+            param -= l_rate * param.grad
+
+
+def make_batch(mcts, network):
+    variation_tensor_list = []
+    target_tensor_list = []
+
+    for child in mcts.root_node.children:
+        mcts.position.push(child.move)
+
+        variation = network.board_to_tensor(mcts.position)
+        target = torch.as_tensor([child.score / child.visits], dtype=torch.float32)
+
+        variation_tensor_list.append(variation)
+        target_tensor_list.append(target)
+
+        mcts.position.pop()
+
+    board_tensor = torch.concatenate(variation_tensor_list).to(torch.float32)
+    target_tensor = torch.concatenate(target_tensor_list).to(torch.float32).view(-1, 1)
+
+    return (board_tensor, target_tensor)
+
+
+def print_analysis(node, intuition):
+    print("Move analysis:")
+    for i, child in enumerate(node.children):
+        ratio = child.score / child.visits
+        move = child.move.uci()
+        intuited = intuition[i].item()
+        print(f"{move}\t{child.visits}\t{ratio:.3f}\t{intuited:.3f}")
+
+
+if __name__ == "__main__":
+    loss_fn = torch.nn.MSELoss()
+    output_filename = "models/sc_cnn.pt"
+    l_rate = 1e-5
+
+    board = chess.Board()
+
+    model = torch.load("models/cnn.pt", weights_only=False)
+    model.eval()
+    network = ConvNetwork(model)
+    mcts = MCTS(board, 300, UCT(1000), network)
+
+    while not mcts.position.is_game_over():
+        choice = mcts.get_move()
+
+        batch = make_batch(mcts, network)
+        train_epoch(model, batch, loss_fn, l_rate)
+
+        print_analysis(mcts.root_node, model(batch[0]))
+
+        mcts.add_move(choice)
+
+        print()
+        print(mcts.position)
+
+        torch.save(network.model, output_filename)
