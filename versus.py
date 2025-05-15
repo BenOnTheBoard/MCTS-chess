ebdd770633deb280e619f5664d6ec99998184559
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-05-15 16:02:07 +0100

    Adds Principal Variation Printing

--- versus.py
+++ versus.py
@@ -5,6 +5,7 @@ import torch
 from engine.heuristics.networks.dualPathNetwork import DualPathNetwork
 from engine.mcts import MCTS
 from engine.treeEvaluators.UCT import UCT
+from engine.utils import node_comparator
 
 
 # Versus Stockfish
@@ -21,6 +22,22 @@ def print_analysis(mcts):
         print(f"{child.move.uci()}\t{child.visits}\t{(child.score / child.visits):.2f}")
 
 
+def print_principal_variation(mcts, is_white):
+    node = mcts.root_node
+    moves = []
+    turn = is_white
+    while not node.is_leaf():
+        node.children.sort(key=node_comparator)
+        if turn:
+            node = node.children[-1]
+        else:
+            node = node.children[0]
+        moves.append(node.move)
+        turn = not turn
+
+    print(mcts.position.variation_san(moves))
+
+
 if __name__ == "__main__":
     stockfish = chess.engine.SimpleEngine.popen_uci(
         r"stockfish\stockfish-windows-x86-64-avx2.exe"
@@ -31,9 +48,11 @@ if __name__ == "__main__":
     model = torch.load("models/dpn.pt", weights_only=False)
     model.eval()
 
-    mcts = MCTS(board, 20, UCT(3), DualPathNetwork(model))
+    mcts = MCTS(board, 40, UCT(3), DualPathNetwork(model))
     while not mcts.position.is_game_over():
         white_choice = mcts.get_move()
+        print_principal_variation(mcts, True)
+        print_analysis(mcts)
         mcts.add_move(white_choice)
 
         print()
