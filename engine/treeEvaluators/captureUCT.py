30770087d2651c9c0b06938846a8f51765f52a2f
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-06-12 01:27:57 +0100

    capture UCT bug, very dumb of me

--- engine/treeEvaluators/captureUCT.py
+++ engine/treeEvaluators/captureUCT.py
@@ -8,13 +8,13 @@ class CaptureUCT(EvaluatorInterface):
         self.C = C
         self.capture_bonus = capture_bonus
 
-    def prior(self, node, state):
-        if state.is_capture(node.move):
+    def prior(self, child, state):
+        if state.is_capture(child.move):
             return 1 + self.capture_bonus
         return 1
 
     def evaluate(self, child, node, state):
-        p = self.prior(node, state)
+        p = self.prior(child, state)
         exploring_term = self.C * p * sqrt(node.visits / (1 + child.visits))
 
         if node.turn:
