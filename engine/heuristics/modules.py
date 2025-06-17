e8d74131862e5b27b237d7f13bc8af6a34069931
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-06-18 00:13:23 +0100

    module block definitions

---
+++ engine/heuristics/modules.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+
+class GlobalSoftmax(nn.Module):
+    def forward(self, x):
+        og_shape = x.shape
+        x_flat = x.view(-1)
+        x_soft = nn.functional.softmax(x_flat, dim=0)
+        return x_soft.view(og_shape)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+
+    def forward(self, x):
+        out = self.bn1(self.conv1(x))
+        out = self.relu(out)
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
