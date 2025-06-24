683b79f658089afff248e4ff03124db1e431a3b4
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-06-24 01:00:45 +0100

    fixing node class for tanh returns and not controlling expansion

--- engine/node.py
+++ engine/node.py
@@ -8,15 +8,9 @@ class Node:
         self.quality = 0
         self.visits = 0
 
-    def expand_node(self, state):
-        if self.children is None and state.outcome(claim_draw=True) is None:
-            self.children = [
-                Node(move, not self.turn, self) for move in state.legal_moves
-            ]
-
     def update_quality(self, quality):
-        if quality > 1 or quality < 0:
-            raise ValueError("Qualities must be between 0 and 1.")
+        if quality > 1 or quality < -1:
+            raise ValueError("Qualities must be between -1 and 1.")
         self.quality = quality
         self.visits += 1
 
