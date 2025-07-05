00edd8c373b3bc5e4cfbe38af68d14e9a7d28ed1
Ben Whitehead <ben.pi.whitehead@gmail.com>
2025-07-05 18:21:05 +0100

    updating self play with mcts analysis

--- selfPlay.py
+++ selfPlay.py
@@ -1,15 +1,19 @@
-import chess
+import bulletchess
+from bulletchess import QUEEN, PAWN, SQUARES, CHECKMATE, DRAW, PIECE_TYPES
 from collections import deque
 import torch
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 
-from engine.heuristics.simpleResNetPolicy import SimpleResNetPolicy
+from engine.backpropagationRules.meanChild import MeanChild
+from engine.heuristics.dualHeadNetwork import DualHeadNetwork
+from engine.mcts import MCTS
+from engine.treeEvaluators.PUCT import PUCT
 from engine.values import DIRECTIONS, KNIGHTS_MOVES
 
 
 class PolicyBoardDataset(Dataset):
-    def __init__(self, board_conversion, move_conversion, max_size=2048):
+    def __init__(self, board_conversion, move_conversion, max_size=2**15):
         self.board_conversion = board_conversion
         self.move_conversion = move_conversion
         self.buffer = deque(maxlen=max_size)
@@ -17,34 +21,39 @@ class PolicyBoardDataset(Dataset):
     def __len__(self):
         return len(self.buffer)
 
-    def add_pair(self, board, move):
-        self.buffer.append((board, move))
+    def add_triple(self, board, result, move):
+        self.buffer.append((board, result, move))
 
     def __getitem__(self, idx):
-        board, move = self.buffer[idx]
+        board, result, move = self.buffer[idx]
         board_tsr = self.board_conversion(board)
-        y = self.move_conversion(move)
-        return board_tsr, torch.tensor(y, dtype=torch.long)
+        y_move = torch.tensor(self.move_conversion(move), dtype=torch.long)
+        y_value = torch.tensor(result, dtype=torch.float32)
+        return board_tsr, y_value, y_move
 
 
-def process_batch(network, batch, loss_fn):
-    board_tensor, targets = batch
+def process_batch(network, batch, value_loss_fn, policy_loss_fn):
+    board_tensor, target_values, target_moves = batch
     batch_size = len(board_tensor)
 
-    board_tensor = board_tensor.to(torch.float32)
+    board_tensor = board_tensor.to(dtype=torch.float32)
     board_tensor = board_tensor.view(batch_size, 11, 8, 8)
 
-    predictions = network.model(board_tensor).view(batch_size, 4672)
-    loss = loss_fn(predictions, targets)
+    predicted_values, predicted_moves = network.model(board_tensor)
+    predicted_moves = predicted_moves.view(batch_size, 4672)
+    predicted_values = predicted_values.view(-1)
 
-    return loss
+    value_loss = value_loss_fn(predicted_values, target_values)
+    policy_loss = policy_loss_fn(predicted_moves, target_moves)
+
+    return value_loss, policy_loss
 
 
 def tensor_to_move(board, distribution):
     flat_index = torch.multinomial(distribution.flatten(), num_samples=1).item()
     plane, location = divmod(flat_index, 64)
     row, col = divmod(location, 8)
-    from_square = row * 8 + col
+    from_square = SQUARES[row * 8 + col]
 
     if plane < 56:
         dir_idx = plane // 7
@@ -53,8 +62,8 @@ def tensor_to_move(board, distribution):
         to_row = row + dr * dist
         to_col = col + dc * dist
 
-        if to_row in (0, 7) and board.piece_type_at(from_square) == chess.PAWN:
-            promotion = chess.QUEEN
+        if to_row in (0, 7) and board[from_square] == PAWN:
+            promotion = QUEEN
         else:
             promotion = None
 
@@ -68,42 +77,53 @@ def tensor_to_move(board, distribution):
     else:
         prom_plane = plane - 64
         prom_idx, dir_type = divmod(prom_plane, 3)
-        to_row = row + 1
+        if row == 1:
+            to_row = 0
+        elif row == 6:
+            to_row = 7
+        else:
+            raise ValueError("Promotion has bad rank")
         to_col = col + dir_type - 1
-        promotion = prom_idx + 2
+        promotion = PIECE_TYPES[prom_idx + 1]
 
-    to_square = to_row * 8 + to_col
-    return chess.Move(from_square, to_square, promotion=promotion)
+    to_square = SQUARES[to_row * 8 + to_col]
+
+    return bulletchess.Move(from_square, to_square, promote_to=promotion)
 
 
 def play_game(network):
+    board = bulletchess.Board()
+    history = []
     while True:
-        board = chess.Board()
-        history = {chess.WHITE: [], chess.BLACK: []}
-        outcome = None
-        while outcome is None:
-            distribution = network.get_masked_move_distribution(board)
-            choice = tensor_to_move(board, distribution)
+        value, distribution = network.get_masked_move_distribution(board)
 
-            history[board.turn].append((board.copy(), choice))
-            board.push(choice)
+        choice = tensor_to_move(board, distribution)
 
-            outcome = board.outcome(claim_draw=True)
+        history.append(board.copy())
+        board.apply(choice)
 
-        if outcome.winner is not None:
-            return history[outcome.winner]
+        if abs(value) > 0.99:
+            return history
+
+        if board in CHECKMATE or board in DRAW:
+            return history
 
 
 def main():
-    model = None
-    network_type = SimpleResNetPolicy
+    model = torch.load("models/dhn.pt", weights_only=False, map_location="cpu")
+    network_type = DualHeadNetwork
     network = network_type(model=model)
 
-    output_filename = "models/new_srnp.pt"
-    loss_fn = torch.nn.CrossEntropyLoss(reduction="sum")
+    output_filename = "models/new_dhn.pt"
+    value_loss_fn = torch.nn.MSELoss(reduction="sum")
+    policy_loss_fn = torch.nn.CrossEntropyLoss(reduction="sum")
     games = 100_000
     init_learning_rate = 1e-3
-    batch_size = 32
+    batch_size = 64
+
+    visits_per_evaluation = 1000
+    tree_evaluator = PUCT(1.225)
+    backprop_rule = MeanChild()
 
     dataset = PolicyBoardDataset(
         network_type.board_to_tensor, network_type.move_to_flat_index
@@ -111,26 +131,47 @@ def main():
 
     optimizer = torch.optim.Adam(network.model.parameters(), lr=init_learning_rate)
 
-    for game_id in range(games):
+    print("Setup complete.")
+
+    for game_id in range(1, games + 1):
         network.model.eval()
         history = play_game(network)
-        for board, move in history:
-            dataset.add_pair(board, move)
+
+        mcts = MCTS(
+            bulletchess.Board(),
+            tree_evaluator,
+            network,
+            backprop_rule,
+        )
+
+        for board in tqdm(history, desc=f"Game {game_id} Review"):
+            mcts.set_position(board)
+            best_move = mcts.get_move(node_count=visits_per_evaluation)
+            dataset.add_triple(board, mcts.root_node.quality, best_move)
         train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
         network.model.train()
-        total_loss = 0
-        for batch in tqdm(train_loader):
+        total_value_loss = 0
+        total_policy_loss = 0
+        for batch in tqdm(train_loader, desc=f"Training after game {game_id}"):
             optimizer.zero_grad()
-            loss = process_batch(network, batch, loss_fn)
-            total_loss += loss.item()
-            loss.backward()
+            value_loss, policy_loss = process_batch(
+                network, batch, value_loss_fn, policy_loss_fn
+            )
+            total_value_loss += value_loss.item()
+            total_policy_loss += policy_loss.item()
+            composite_loss = value_loss + policy_loss
+            composite_loss.backward()
             optimizer.step()
 
-        print(f"Game: {game_id}\tTraining Loss: {total_loss / len(dataset)}")
-
         if game_id % 25 == 0:
-            torch.save(network.model, output_filename)
+            print(f"""
+                Games played: {game_id}
+                Value Head Loss: {total_value_loss / len(dataset)}
+                Policy Head Loss: {total_policy_loss / len(dataset)}
+            """)
+
+        torch.save(network.model, output_filename)
 
     print("\n\nTraining is done!")
 
