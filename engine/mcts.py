22d2dd2e2f6b19eae8b43f3f3787440c40554e5b
Ben Whitehead <ben.pi.whitehead@gmail.com>
2026-05-05 01:20:38 +0100

    makes node class smaller

--- engine/mcts.py
+++ engine/mcts.py
@@ -15,8 +15,11 @@ class MCTS:
         self.exploration = exploration
         self.set_position(position)
 
+        self.indexer = self.network.move_to_flat_index
+        self.evaluator = self.network.evaluate
+
     def set_position(self, new_position):
-        self.root_node = Node(None, new_position.turn, None, None)
+        self.root_node = Node(None, None)
         self.position = new_position.copy()
 
     def add_move(self, move):
@@ -24,14 +27,13 @@ class MCTS:
         if self.root_node.children is not None:
             for child in self.root_node.children:
                 if child.move == move:
-                    child.parent = None
                     self.root_node = child
                     return
-        self.root_node = Node(None, ~self.root_node.turn, None, None)
+        self.root_node = Node(None, None)
 
-    def PUCT(self, node):
+    def PUCT(self, node, turn):
         sqrtv = sqrt(node.visits)
-        sign = OUTCOMES[node.turn]
+        sign = OUTCOMES[turn]
 
         def PUCT_node(child):
             exploring_term = sqrtv / (1 + child.visits)
@@ -40,36 +42,29 @@ class MCTS:
 
         return PUCT_node
 
-    def tree_policy(self, node):
+    def tree_policy(self, node, turn):
         for child in node.children:
             if child.visits == 0:
                 return child
 
-        evaluator = self.PUCT(node)
+        evaluator = self.PUCT(node, turn)
         return max(node.children, key=evaluator)
 
-    def expand_node(self, node, state, move_distribution):
+    def expand_node(self, node, state, move_dist):
         legal_moves = tuple(state.legal_moves())
-        flat_dist = move_distribution.flatten()
-        idxs = [self.network.move_to_flat_index(move) for move in legal_moves]
-        probs = softmax(flat_dist[idxs], dim=0)
-        node.children = tuple(
-            Node(m, ~node.turn, p.item(), node) for p, m in zip(probs, legal_moves)
-        )
+        flat_dist = move_dist.flatten()
+        idxs = [self.indexer(move) for move in legal_moves]
+        probs = softmax(flat_dist[idxs], dim=0).tolist()
+        node.children = tuple(Node(m, p) for p, m in zip(probs, legal_moves))
 
     def evaluate_state(self, state):
         cached_pair = self.LRUCache.get(state)
         if cached_pair is not None:
             return cached_pair
 
-        eval_pair = self.network.evaluate(state)
+        eval_pair = self.evaluator(state)
         self.LRUCache.put(state, eval_pair)
         return eval_pair
-
-    def propagate_updates(self, node, value):
-        while node is not None:
-            node.update_quality(value)
-            node = node.parent
 
     def get_move(self, node_count, tqdm_on=False):
         if tqdm_on:
@@ -79,9 +74,11 @@ class MCTS:
 
         for _ in counter:
             node, state = self.root_node, self.position.copy()
-            while not node.is_leaf():
-                node = self.tree_policy(node)
+            path = [node]
+            while node.children is not None:
+                node = self.tree_policy(node, state.turn)
                 state.apply(node.move)
+                path.append(node)
 
             if state in CHECKMATE:
                 result = -OUTCOMES[state.turn]
@@ -91,7 +88,8 @@ class MCTS:
                 result, move_distribution = self.evaluate_state(state)
                 self.expand_node(node, state, move_distribution)
 
-            self.propagate_updates(node, result)
+            for path_node in path:
+                path_node.update_quality(result)
 
         most_visited = max(self.root_node.children, key=lambda n: n.visits)
         return most_visited.move
--- engine/node.py
+++ engine/node.py
@@ -1,19 +1,11 @@
 class Node:
-    def __init__(self, move, turn, prior, parent):
+    def __init__(self, move, prior):
         self.move = move
-        self.turn = turn
-        self.parent = parent
-        self.prior = prior
         self.children = None
+        self.prior = prior
         self.quality = 0
         self.visits = 0
 
     def update_quality(self, value):
         self.visits += 1
         self.quality += (value - self.quality) / self.visits
-
-    def is_leaf(self):
-        return self.children is None
-
-    def has_parent(self):
-        return self.parent is not None
